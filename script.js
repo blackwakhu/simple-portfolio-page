421fff22e5f625a76a7e779b267d25eba6c77952
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-05-13 08:56:49 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $("#redElement").click(function(){
+    $(this).css('color', 'red');
+    });
+    
+    $("#blueElement").click(function(){
+      $(this).css('color', 'blue');
+    });
+    
+    $("#greenElement").click(function(){
+      $(this).css('color', 'green');
+    });
+});
