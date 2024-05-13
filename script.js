8bd0806fe6628f9d7132e303d2d41c90feeebe93
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-05-13 11:49:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
   $("#homebtn").click(function(){
-    $("#home").slideDown(1000);
+    $("#home").slideDown(800);
     $("#product").hide();
+    $("#about").hide();
+    $("#blog").hide();
+    $("#career").hide();
   })
   $("#productbtn").click(function(){
-    $("#product").slideDown(1000);
+    $("#product").slideDown(800);
     $("#home").hide();
+    $("#about").hide();
+    $("#blog").hide();
+    $("#career").hide();
+  })
+  $("#aboutbtn").click(function(){
+    $("#about").slideDown(800);
+    $("#home").hide();
+    $("#product").hide();
+    $("#blog").hide();
+    $("#career").hide();
+  })
+  $("#blogbtn").click(function(){
+    $("#blog").slideDown(800);
+    $("#home").hide();
+    $("#product").hide();
+    $("#about").hide();
+    $("#career").hide();
+  })
+  $("#careerbtn").click(function(){
+    $("#career").slideDown(800);
+    $("#home").hide();
+    $("#product").hide();
+    $("#about").hide();
+    $("#blog").hide();
   })
 })
